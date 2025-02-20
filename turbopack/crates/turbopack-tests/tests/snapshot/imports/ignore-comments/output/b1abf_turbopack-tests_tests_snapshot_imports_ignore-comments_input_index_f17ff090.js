f90d3a77b2a4ca0986cb10176b0c8feed436539b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-20 23:10:39 +0100

    Update snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/imports/ignore-comments/output/b1abf_turbopack-tests_tests_snapshot_imports_ignore-comments_input_index_f17ff090.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/b1abf_turbopack-tests_tests_snapshot_imports_ignore-comments_input_index_f17ff090.js",
+    {},
+    {"otherChunks":["output/4c35f_tests_snapshot_imports_ignore-comments_input_vercel_mjs_87352388._.js","output/4e721_crates_turbopack-tests_tests_snapshot_imports_ignore-comments_input_e1849155._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/imports/ignore-comments/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
