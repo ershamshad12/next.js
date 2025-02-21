249301e56a3bcce2c366f9440a1478cde20e7e99
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-21 13:21:43 +0100

    Use correct ChunkGroups for chunking

--- crates/next-api/src/app.rs
+++ crates/next-api/src/app.rs
@@ -31,7 +31,7 @@ use next_core::{
         get_server_module_options_context, get_server_resolve_options_context,
         get_server_runtime_entries, ServerContextType,
     },
-    next_server_utility::NextServerUtilityTransition,
+    next_server_utility::{NextServerUtilityTransition, NEXT_SERVER_UTILITY_MERGE_TAG},
     parse_segment_config_from_source,
     util::NextRuntime,
 };
@@ -1193,6 +1193,8 @@ impl AppEndpoint {
             client_chunking_context,
             Value::new(client_shared_availability_info),
             ssr_chunking_context,
+            *rsc_entry,
+            project.project_path(),
         );
         let client_references_chunks_ref = client_references_chunks.await?;
 
@@ -1669,43 +1671,54 @@ impl AppEndpoint {
                 evaluatable_assets.push(server_action_manifest_loader);
                 evaluatable_assets.push(rsc_entry);
 
+                let entry_chunk_group = *module_graph
+                    .chunk_group_info()
+                    .get_index_of(ChunkGroup::Entry {
+                        entries: vec![ResolvedVc::upcast(rsc_entry)],
+                        ty: ChunkGroupType::Entry,
+                    })
+                    .await?;
+
                 async {
                     let mut current_chunks = OutputAssets::empty();
                     let mut current_availability_info = AvailabilityInfo::Root;
 
                     let client_references = client_references.await?;
-                    let span = tracing::trace_span!("server utils");
-                    async {
-                        let server_utils = client_references
-                            .server_utils
-                            .iter()
-                            .map(async |m| Ok(ResolvedVc::upcast(m.await?.module)))
-                            .try_join()
-                            .await?;
-                        let chunk_group = chunking_context
-                            .chunk_group(
-                                AssetIdent::from_path(this.app_project.project().project_path())
+                    if !client_references.server_utils.is_empty() {
+                        let span = tracing::trace_span!("server utils");
+                        async {
+                            let server_utils = client_references
+                                .server_utils
+                                .iter()
+                                .map(async |m| Ok(ResolvedVc::upcast(m.await?.module)))
+                                .try_join()
+                                .await?;
+                            let chunk_group = chunking_context
+                                .chunk_group(
+                                    AssetIdent::from_path(
+                                        this.app_project.project().project_path(),
+                                    )
                                     .with_modifier(server_utils_modifier()),
-                                // TODO this should be ChunkGroup::Shared
-                                ChunkGroup::Entry {
-                                    entries: server_utils,
-                                    ty: ChunkGroupType::Entry,
-                                },
-                                module_graph,
-                                Value::new(current_availability_info),
-                            )
-                            .await?;
+                                    ChunkGroup::SharedMerged {
+                                        parent: entry_chunk_group,
+                                        merge_tag: NEXT_SERVER_UTILITY_MERGE_TAG.clone(),
+                                        entries: server_utils,
+                                    },
+                                    module_graph,
+                                    Value::new(current_availability_info),
+                                )
+                                .await?;
 
-                        current_chunks = current_chunks
-                            .concatenate(*chunk_group.assets)
-                            .resolve()
-                            .await?;
-                        current_availability_info = chunk_group.availability_info;
-
-                        anyhow::Ok(())
+                            current_chunks = current_chunks
+                                .concatenate(*chunk_group.assets)
+                                .resolve()
+                                .await?;
+                            current_availability_info = chunk_group.availability_info;
+                            anyhow::Ok(())
+                        }
+                        .instrument(span)
+                        .await?;
                     }
-                    .instrument(span)
-                    .await?;
                     for server_component in client_references
                         .server_component_entries
                         .iter()
@@ -1725,13 +1738,9 @@ impl AppEndpoint {
                             let chunk_group = chunking_context
                                 .chunk_group(
                                     server_component.ident(),
-                                    // TODO this should be ChunkGroup::Shared
-                                    ChunkGroup::Entry {
-                                        entries: vec![ResolvedVc::upcast(
-                                            server_component.await?.module,
-                                        )],
-                                        ty: ChunkGroupType::Entry,
-                                    },
+                                    ChunkGroup::Shared(ResolvedVc::upcast(
+                                        server_component.await?.module,
+                                    )),
                                     module_graph,
                                     Value::new(current_availability_info),
                                 )
