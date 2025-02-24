fed376836e93023cb5f1825bc20c3ea581544a3a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-24 11:28:27 +0100

    Fix edge chunking

--- crates/next-api/src/app.rs
+++ crates/next-api/src/app.rs
@@ -1651,25 +1651,35 @@ impl AppEndpoint {
 
         Ok(match runtime {
             NextRuntime::Edge => {
+                let ChunkGroupResult {
+                    assets,
+                    availability_info,
+                } = *chunking_context
+                    .evaluated_chunk_group(
+                        server_action_manifest_loader.ident(),
+                        Vc::cell(vec![server_action_manifest_loader]),
+                        module_graph,
+                        Value::new(AvailabilityInfo::Root),
+                    )
+                    .await?;
+
                 let mut evaluatable_assets =
                     this.app_project.edge_rsc_runtime_entries().owned().await?;
                 let evaluatable = ResolvedVc::try_sidecast(app_entry.rsc_entry)
                     .context("Entry module must be evaluatable")?;
                 evaluatable_assets.push(evaluatable);
-                evaluatable_assets.push(server_action_manifest_loader);
 
-                {
-                    let _span = tracing::info_span!("Server Components");
+                assets.concatenate(
                     chunking_context
                         .evaluated_chunk_group_assets(
                             app_entry.rsc_entry.ident(),
                             Vc::cell(evaluatable_assets.clone()),
                             module_graph,
-                            Value::new(AvailabilityInfo::Root),
+                            Value::new(availability_info),
                         )
                         .resolve()
-                        .await?
-                }
+                        .await?,
+                )
             }
             NextRuntime::NodeJs => {
                 let Some(rsc_entry) = ResolvedVc::try_downcast(app_entry.rsc_entry) else {
@@ -1939,9 +1949,15 @@ impl Endpoint for AppEndpoint {
 
     #[turbo_tasks::function]
     async fn entries(self: Vc<Self>) -> Result<Vc<GraphEntries>> {
+        let app_entry = self.app_endpoint_entry().await?;
+        let runtime = app_entry.config.await?.runtime.unwrap_or_default();
+
         Ok(Vc::cell(vec![(
-            vec![self.app_endpoint_entry().await?.rsc_entry],
-            Some(ChunkGroupType::Entry),
+            vec![app_entry.rsc_entry],
+            Some(match runtime {
+                NextRuntime::NodeJs => ChunkGroupType::Entry,
+                NextRuntime::Edge => ChunkGroupType::Evaluated,
+            }),
         )]))
     }
 
@@ -1983,7 +1999,10 @@ impl Endpoint for AppEndpoint {
 
         Ok(Vc::cell(vec![(
             vec![server_actions_loader],
-            Some(ChunkGroupType::Entry),
+            Some(match runtime {
+                NextRuntime::NodeJs => ChunkGroupType::Entry,
+                NextRuntime::Edge => ChunkGroupType::Evaluated,
+            }),
         )]))
     }
 }
